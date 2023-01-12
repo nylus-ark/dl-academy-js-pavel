35628b989e5d5943a09522218a6c6746c85d4ccb
nylus-ark <nylus-ark@users.noreply.github.com>
2023-01-12 15:43:39 +0000

    deploy: 0160b7f02efc5cb216751d72faaf16400f97cb23

--- homework/02/01.js
+++
@@ -1,4 +0,0 @@
-const userInput = getUserInput('Введите любое целое число');
-const userInputArr = [...Array(Number(userInput)).keys()];
-
-console.log(userInputArr.map(number => ++number).filter((number) => number % 4 !== 0));
